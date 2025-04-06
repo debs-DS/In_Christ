781786e2642dcf81b4da757aa372be2dc53714d3
debs-DS <deborahtunde9@gmail.com>
2025-04-07 00:20:49 +0100

    78

--- text.js
+++ text.js
@@ -1,10 +1,10 @@
     // nav bar
-    function color(x) {x.style.color = "lightcoral"}
+    function color(x) {x.style.color = "#6495ed"}
     function black(x) {x.style.color = "black"}
 
     // button
     function fade(x) {x.style.backgroundColor = "grey", x.style.color = "white"}
-    function coral(x) {x.style.backgroundColor = "lightcoral", x.style.color = "black"}
+    function coral(x) {x.style.backgroundColor = "#6495ed", x.style.color = "black"}
     function white(x) {x.style.color = "white"}
 
     // march green
